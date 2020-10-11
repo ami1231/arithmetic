35679549730915125fb3a7a823058c23619da39e
ami <domybstforyou.tsu@gmail.com>
2020-10-12 01:21:23 +0800

    插入排序 未完成

--- src/main/java/com/ami/arithmetic/sort/insertion/InsertionMain.java
+++ src/main/java/com/ami/arithmetic/sort/insertion/InsertionMain.java
@@ -1,9 +1,14 @@
 package com.ami.arithmetic.sort.insertion;
 
+import java.util.Arrays;
+
 public class InsertionMain {
 
     public static void main(String[] args){
-
+        Integer[] sortArray = new Integer[]{3,4,1,9,0,0,1,2,1,5,8,8,7};
+        InsertionSort<Integer> insertionSort = new InsertionSort<>();
+        sortArray = insertionSort.sort(sortArray);
+        System.out.println(Arrays.asList(sortArray));
     }
 
 }
--- src/main/java/com/ami/arithmetic/sort/insertion/InsertionSort.java
+++ src/main/java/com/ami/arithmetic/sort/insertion/InsertionSort.java
@@ -1,8 +1,39 @@
 package com.ami.arithmetic.sort.insertion;
 
-public class InsertionSort<T> {
+import java.util.Arrays;
 
-    public T[] sort(T[] sortArray,T obj){
+public class InsertionSort<M extends Comparable<M>> {
+
+
+//    Integer[] sortArray = new Integer[]{3,4,1,9,0,0,1,2,1,5,8,8,7};
+    public M[] sort(M[] sortArray){
+
+        int length = sortArray.length;
+
+        for(int i=0;i<length;i++){
+            System.out.println("============>>>");
+            if(i==0){
+                continue;
+            }
+            M sortObj = sortArray[i];
+            for(int j=0;j<=i;j++){
+                System.out.println(sortObj);
+                M baseObj = sortArray[j];
+                System.out.println(baseObj);
+                if(sortObj.compareTo(baseObj)<0){
+                    M tempObj = sortObj;
+
+                    for(int k=j;k<=i;k++){
+                        sortArray[k+1] = sortArray[k];
+                    }
+                    sortArray[j] = tempObj;
+                    break;
+                }
+            }
+            System.out.println("============<<<");
+            System.out.println(Arrays.asList(sortArray));
+        }
+
         return sortArray;
     }
 
